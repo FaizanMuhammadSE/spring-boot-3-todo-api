f5fa0b816ce919fd04f408035ddab1d2da07e102
FaizanMuhammadSE <jutt41697@gmail.com>
2023-11-28 15:57:09 +0500

    DAO layer | Get endpoint added
    -DAO layer added which will interact with Database, DAO is extending JPA object to make CURD operations easy
    
    -we gave our api a path, using RequestMapping Annotation
    -Then we annotate a method as Get so it will respond and return all Todos

--- src/main/java/com/simpleproject/Main.java
+++ src/main/java/com/simpleproject/Main.java
@@ -3,17 +3,28 @@ package com.simpleproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/todo")
 public class Main {
+    private final TodoRepository todoRepository;
+
+    public Main(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public String greet() {
-        return "Namaste Spring Boot, I'm Here";
+    @GetMapping
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
     }
+
 }
